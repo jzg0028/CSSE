1f943beb19e5ae1419936817415769bfceaa72ee
Jesse <jzg0028@aubrun.edu>
2017-02-02 20:27:03 -0600

    first working release of convertString2Dictionary

--- softwareprocess/convertString2Dictionary.py
+++ softwareprocess/convertString2Dictionary.py
@@ -1,5 +1,14 @@
-import urllib
+"""
+    Jesse 2/2/17
+"""
 
-def convertString2Dictionary(inputString = ""):
-    errorDict = {'error':'true'}
-    return errorDict
+import urllib
+import re
+
+def convertString2Dictionary(inputString = ''):
+    inputString = urllib.unquote(inputString)
+    return (dict(re.compile('([a-zA-Z]\w*)\s*=\s*(\w+)').findall(inputString))
+        if re.compile
+        ('^\s*[a-zA-Z]\w*\s*=\s*\w+(?:\s*,\s*[a-zA-Z]\w*\s*=\s*\w+)+\s*$')
+        .match(inputString)
+        else {'error' : 'true'})

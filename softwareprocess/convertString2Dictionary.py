0cae8573aabc0521a2132d4ec213895ecd104371
Jesse <jzg0028@aubrun.edu>
2017-02-05 22:12:04 -0600

    fixed convertString2Dictionary for duplicate keys

--- softwareprocess/convertString2Dictionary.py
+++ softwareprocess/convertString2Dictionary.py
@@ -1,14 +1,23 @@
 """
     Jesse 2/2/17
+    Jesse 2/5/17
 """
 
 import urllib
 import re
+from collections import Counter
 
 def convertString2Dictionary(inputString = ''):
     inputString = urllib.unquote(inputString)
-    return (dict(re.compile('([a-zA-Z]\w*)\s*=\s*(\w+)').findall(inputString))
-        if re.compile
-        ('^\s*[a-zA-Z]\w*\s*=\s*\w+(?:\s*,\s*[a-zA-Z]\w*\s*=\s*\w+)+\s*$')
-        .match(inputString)
-        else {'error' : 'true'})
+    # if the string is valid
+    if re.compile(r'\s*[a-zA-Z]\w*\s*=\s*\w+\s*'
+        r'(?:,\s*[a-zA-Z]\w*\s*=\s*\w+\s*)+$').match(inputString):
+        # find all key/value pairs
+        o = re.compile(r'([a-zA-Z]\w*)\s*=\s*(\w+)').findall(inputString)
+        # if there are no duplicates
+        if all(count == 1 for _, count
+            in Counter(key for key, _ in o).iteritems()):
+            # return a dictionary
+            return dict(o)
+    # return the error
+    return {'error' : 'true'}

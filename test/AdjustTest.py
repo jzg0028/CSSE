43d576106bc42e13dcdd975ae6c19a34ff9c79c8
Jesse <jzg0028@aubrun.edu>
2017-03-12 22:18:36 -0500

    Red Light |

--- test/AdjustTest.py
+++ test/AdjustTest.py
@@ -3,8 +3,19 @@ import softwareprocess.adjust as adjust
 
 class AngleTest(unittest.TestCase):
 
-    def test_invalid_observationMissing(self):
+    def test_observationMissingError(self):
         self.assertTrue('error' in adjust.adjust({}))
 
-    def test_valid_observationPresent(self):
-        self.assertTrue(not 'error' in adjust.adjust({'observation' : '0d0.0'}))
+    def test_observationPresentNotError(self):
+        self.assertTrue(not 'error' in adjust.adjust({'observation' : '5d0.0'}))
+
+    def test_observationLowBoundError(self):
+        self.assertTrue('error' in adjust.adjust({'observation' : '0d0.0'}))
+
+    def test_observationFormatError(self):
+        self.assertTrue('error' in adjust.adjust({'observation' : '0d0'}))
+        self.assertTrue('error' in adjust.adjust({'observation' : '0d.0'}))
+        self.assertTrue('error' in adjust.adjust({'observation' : '0d0.'}))
+        self.assertTrue('error' in adjust.adjust({'observation' : 'd0.0'}))
+        self.assertTrue('error' in adjust.adjust({'observation' : '0y0.0'}))
+        self.assertTrue('error' in adjust.adjust({'observation' : '00.0'}))

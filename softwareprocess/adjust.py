c9cbc07f67c3927bf5a32bdc1ca057f0aac89f2b
Jesse <jzg0028@aubrun.edu>
2017-03-12 22:05:01 -0500

    Green Light |

--- softwareprocess/adjust.py
+++ softwareprocess/adjust.py
@@ -1,4 +1,12 @@
+import softwareprocess.angle as angle
+
 def adjust(values):
     if not 'observation' in values:
         values['error'] = 'mandatory information is missing'
+        return values
+    observation = angle.parse(values['observation'])
+    # FIX this is inefficient, but easy for now
+    if observation < angle.parse('0d0.1'):
+        values['error'] = 'observation is below 0d0.1'
+        return values
     return values

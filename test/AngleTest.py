0eadd68cf16b18fe70bbc8cc3fb1b96482cdd8ac
Jesse <jzg0028@aubrun.edu>
2017-03-12 20:36:13 -0500

    Red Light |

--- test/AngleTest.py
+++ test/AngleTest.py
@@ -7,4 +7,10 @@ class AngleTest(unittest.TestCase):
         self.assertEqual('0d0.0', angle.toString(0))
         self.assertEqual('0d30.0', angle.toString(0.5))
         self.assertEqual('30d30.0', angle.toString(30.5))
-        self.assertEqual('90d0.0', angle.toString(90))
+        self.assertEqual('90d0.0', angle.toString(90.0))
+
+    def test_valid_parse(self):
+        self.assertAlmostEqual(0.0, angle.parse('0d0.0'))
+        self.assertAlmostEqual(0.5, angle.parse('0d30.0'))
+        self.assertAlmostEqual(30.5, angle.parse('30d30.0'))
+        self.assertAlmostEqual(90.0, angle.parse('90d0.0'))

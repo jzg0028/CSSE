3f8a76669efa28892f281727c5457e0145d22a74
Jesse <jzg0028@aubrun.edu>
2017-03-12 20:52:47 -0500

    Green Light |

--- softwareprocess/angle.py
+++ softwareprocess/angle.py
@@ -1,2 +1,9 @@
+import re
+
 def toString(angle):
     return '%dd%.1f' % (int(angle), (angle % 1) * 60)
+
+def parse(angle):
+    match = re.match('^(\d{1,3})d(\d{1,2}\.\d)$', angle)
+
+    return int(match.group(1)) + float(match.group(2)) / 60

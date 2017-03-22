ceee8ab42f046d9624775797254a4a873aca4de9
Jesse <jzg0028@aubrun.edu>
2017-03-22 18:06:04 -0500

    Green Light |

--- softwareprocess/angle.py
+++ softwareprocess/angle.py
@@ -4,10 +4,19 @@ def toString(angle):
     return '%dd%.1f' % (int(angle), (angle % 1) * 60)
 
 def parse(angle):
+    return parseDegrees(angle) + parseMinutes(angle)
+
+def parseDegrees(angle):
+    return int(capture(angle)[0])
+
+def parseMinutes(angle):
+    return float(capture(angle)[1]) / 60
+
+def capture(angle):
     match = re.match('^(\d{1,3})d(\d{1,2}\.\d)$', angle)
     if not match:
         raise ValueError('invalid angle string format: ' + angle)
-    return int(match.group(1)) + float(match.group(2)) / 60
+    return (match.group(1), match.group(2));
 
 def normalize(angle, low, high):
     total = abs(low) + abs(high)

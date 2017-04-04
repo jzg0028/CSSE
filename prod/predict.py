3373eeaf558149b59ab415dcee9d0417f357baf9
Jesse <jzg0028@aubrun.edu>
2017-04-03 22:46:24 -0500

    Green Light |

--- prod/predict.py
+++ prod/predict.py
@@ -2,13 +2,16 @@ from stars import stars
 from datetime import datetime
 
 def predict(values):
+    error = None
     if 'body' not in values:
         error = 'body not present'
     elif values['body'] not in (i[0] for i in stars):
         error = 'invalid body'
 
+    date = '2001-01-01' if 'date' not in values else values['date']
+
     try:
-        datetime.strptime(values['date'], '%Y-%m-%d')
+        datetime.strptime(date, '%Y-%m-%d')
     except Exception as e:
         error = str(e)
         

17a6fae61e121de49d015444ad0b737cd7055254
Jesse <jzg0028@aubrun.edu>
2017-04-03 21:59:45 -0500

    Green Light |

--- prod/predict.py
+++ prod/predict.py
@@ -1,4 +1,8 @@
+from stars import stars
+
 def predict(values):
-    if not 'body' in values:
+    if 'body' not in values:
         values['error'] = 'body not present'
+    elif values['body'] not in (i[0] for i in stars):
+        values['error'] = 'invalid body'
     return values

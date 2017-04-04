f32161e381e3110e96b2f9c3fde8cb3c0c478137
Jesse <jzg0028@aubrun.edu>
2017-04-04 10:14:00 -0500

    Green Light |

--- prod/predict.py
+++ prod/predict.py
@@ -1,5 +1,6 @@
 from stars import stars
 from datetime import datetime
+import angle
 
 def predict(values):
     error = None
@@ -21,17 +22,12 @@ def predict(values):
         values['error'] = error
     return values
 
-def aries(date, time):
-    observation = None
-    try:
-        observation.strptime(date + ' ' + time, '%Y-%m-%d %H:%M:%S')
-    except Exception as e:
-        raise ValueError('bad date or time format: ' + e)
-    reference = datetime(2001, 1, 1)
-
 def countLeapYears(fromYear, toYear):
     out = 0
     for year in xrange(fromYear, toYear):
         out += 1 if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0) \
             else 0
     return out
+
+def angularDifference(fromYear, toYear):
+    return (toYear - fromYear) * angle.parse('0d14.31667') * -1

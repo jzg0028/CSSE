057e8ec7b76e82ba16ab256c136f86f5686e6768
Jesse <jzg0028@aubrun.edu>
2017-04-22 12:56:07 -0500

    Red Light |

---
+++ test/CorrectionTest.py
@@ -0,0 +1,40 @@
+import unittest
+from prod.correct import Correction
+from prod.angle import Angle
+import sys
+
+class CorrestionTest(unittest.TestCase):
+
+    def testLatitudeOutOfLowBound(self):
+        try:
+            Correction(Angle.parse('-90d0.0'), Angle.parse('0d0.0'),
+                Angle.parse('1d0.0'), Angle.parse('0d0.0'),
+                Angle.parse('0d0.0'))
+            self.fail('latitude out of bounds didn\'t raise exception')
+        except ValueError:
+            sys.exc_clear()
+
+    def testLatitudeWithinLowBound(self):
+        try:
+            Correction(Angle.parse('-90d0.1'), Angle.parse('0d0.0'),
+                Angle.parse('1d0.0'), Angle.parse('0d0.0'),
+                Angle.parse('0d0.0'))
+        except ValueError:
+            self.fail('latitude within bounds raised exception')
+
+    def testLatitudeOutOfHighBound(self):
+        try:
+            Correction(Angle.parse('90d0.0'), Angle.parse('0d0.0'),
+                Angle.parse('1d0.0'), Angle.parse('0d0.0'),
+                Angle.parse('0d0.0'))
+            self.fail('latitude out of bounds didn\'t raise exception')
+        except ValueError:
+            sys.exc_clear()
+
+    def testLatitudeWithinHighBound(self):
+        try:
+            Correction(Angle.parse('89d59.9'), Angle.parse('0d0.0'),
+                Angle.parse('1d0.0'), Angle.parse('0d0.0'),
+                Angle.parse('0d0.0'))
+        except ValueError:
+            self.fail('latitude within bounds raised exception')

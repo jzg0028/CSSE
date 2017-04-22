3900d104819f7744cc144f4dbdc30901260754fc
Jesse <jzg0028@aubrun.edu>
2017-04-22 13:04:26 -0500

    Green Light |

--- prod/correct.py
+++ prod/correct.py
@@ -10,8 +10,8 @@ class Correction(object):
         self.setAssumedLongitude(assLon)
 
     def setLatitude(self, lat):
-        if lat.getDegrees() <= -90 \
-            or lat.getDegrees() >= 90 \
+        if float(lat) <= -90.0 \
+            or float(lat) >= 90.0 \
             or lat.getMinutes() < 0 \
             or lat.getMinutes() >= 60:
             raise ValueError('latitude out of range: %s' % str(lat))
@@ -21,8 +21,8 @@ class Correction(object):
         return self.lat
 
     def setLongitude(self, lon):
-        if lon.getDegrees() < 0 \
-            or lon.getDegrees() >= 360 \
+        if float(lon) < 0.0 \
+            or float(lon) >= 360.0 \
             or lon.getMinutes() < 0 \
             or lon.getMinutes() >= 60:
             raise ValueError('longitude out of range: %s' % str(lon))
@@ -32,8 +32,8 @@ class Correction(object):
         return self.lon
 
     def setAltitude(self, alt):
-        if alt.getDegrees() <= 0 \
-            or alt.getDegrees() >= 90 \
+        if float(alt) <= 0.0 \
+            or float(alt) >= 90.0 \
             or alt.getMinutes() < 0 \
             or alt.getMinutes() >= 60:
             raise ValueError('altitude out of range: %s' % str(alt))
@@ -43,8 +43,8 @@ class Correction(object):
         return self.alt
 
     def setAssumedLatitude(self, assLat):
-        if assLat.getDegrees() <= -90 \
-            or assLat.getDegrees() >= 90 \
+        if float(assLat) <= -90.0 \
+            or float(assLat) >= 90.0 \
             or assLat.getMinutes() < 0 \
             or assLat.getMinutes() >= 60:
             raise ValueError('assumed latitude out of range: %s' % str(assLat))
@@ -54,8 +54,8 @@ class Correction(object):
         return self.assLat
 
     def setAssumedLongitude(self, assLon):
-        if assLon.getDegrees() < 0 \
-            or assLon.getDegrees() >= 360 \
+        if float(assLon) < 0.0 \
+            or float(assLon) >= 360.0 \
             or assLon.getMinutes() < 0 \
             or assLon.getMinutes() >= 60:
             raise ValueError('assumed longitude out of range: %s' %
